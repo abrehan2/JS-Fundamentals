cdbfdd05d7191657309b1aa75099ab3bb4f856e0
abrehan2 <rehannajam2@gmail.com>
2022-05-03 19:29:00 +0500

    first commit

---
+++ p-1/assets/file.js
@@ -0,0 +1,18 @@
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach(function(panel)
+{
+panel.addEventListener("click", function()
+{
+    remove_active();
+panel.classList.add("active");
+});
+});
+
+function remove_active()
+{
+    panels.forEach(function(panel)
+    {
+panel.classList.remove("active");
+    });
+}
