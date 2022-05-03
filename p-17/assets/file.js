22a4fd2f6b2054288b5dff52d8f30cc4052b2e67
abrehan2 <rehannajam2@gmail.com>
2022-05-03 19:19:22 +0500

    first commit

---
+++ p-17/assets/file.js
@@ -0,0 +1,34 @@
+const btn = document.getElementById('button');
+const toasts = document.getElementById('toasts');
+
+const messages = [
+'Message One',
+'Message Two',
+'Message Three',
+'Message Four',
+]
+
+btn.addEventListener('click', () => Notification('Message'))
+
+
+function Notification(x = null)
+{
+    const notif = document.createElement('div')
+    notif.classList.add('toast');
+
+    notif.innerText = x ? x : random_message();
+
+    toasts.appendChild(notif);
+    
+    setTimeout(() => {
+        notif.remove(); // remove from dom
+    }, 3000);
+
+
+
+}
+
+function random_message()
+{
+  return messages[Math.floor(Math.random() * messages.length)];  
+}
