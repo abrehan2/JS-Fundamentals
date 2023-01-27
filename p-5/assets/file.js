7df54a4a087886c4fdad10f1d8372b141973452b
Abdul Rehan <100872683+abrehan2@users.noreply.github.com>
2023-01-27 21:58:29 +0500

    first commit

--- p-5/assets/file.js
+++ p-5/assets/file.js
@@ -2,8 +2,7 @@ const labels  = document.querySelectorAll(".form-control label");
 
 labels.forEach(label => {
     label.innerHTML = label.innerText
-
     .split("")
     .map((letter, idx) => `<span style= "transition-delay: ${idx * 100}ms"> ${letter} </span>`)
-    .join('')
+    .join('');
 })
